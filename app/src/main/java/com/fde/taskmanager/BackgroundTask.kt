a61c85bd4d370d384d7a1e975cee80e620a46bfc
ColdRiver <17830831606@163.com>
2025-10-16 15:17:01 +0800

    fix: keeping the data of ProcessView and ResourceView as backend tasks and adding page maintenance in NavController limits the maximum height of ResourceView's disk and network charts

--- app/src/main/java/com/fde/taskmanager/BackgroundTask.kt
+++ app/src/main/java/com/fde/taskmanager/BackgroundTask.kt
@@ -1,6 +1,7 @@
 package com.fde.taskmanager
 
 import android.util.Log
+import androidx.compose.runtime.toMutableStateList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -19,8 +20,143 @@ object BackgroundTask {
     private val _taskInfoList = MutableStateFlow<List<Adapters.TaskInfo>>(emptyList())
     val taskInfoList: StateFlow<List<Adapters.TaskInfo>> = _taskInfoList.asStateFlow()
 
+    private var _cpuPercentState: MutableStateFlow<List<List<Float>>> = MutableStateFlow(emptyList())
+    val cpuPercentState: StateFlow<List<List<Float>>> = _cpuPercentState.asStateFlow()
+
+    private var _memoryAndSwapList: MutableStateFlow<List<List<Float>>> = MutableStateFlow(listOf(emptyList(),emptyList()))
+    val memoryAndSwapList: StateFlow<List<List<Float>>> = _memoryAndSwapList.asStateFlow()
+
+    private val _memoryAndSwap = MutableStateFlow<Adapters.MemoryInfo>(
+        Adapters.MemoryInfo(
+            Adapters.MemoryInfo.MemoryDetail(
+                0f,0,0,0
+            ),
+            Adapters.MemoryInfo.SwapDetail(
+                0f,0,0
+            )
+        )
+    )
+    val memoryAndSwap: StateFlow<Adapters.MemoryInfo> = _memoryAndSwap.asStateFlow()
+
+
+    private val _networkStatsState = MutableStateFlow<Adapters.NetworkStats>(
+        Adapters.NetworkStats(
+            Adapters.NetworkStats.TransferStats(
+                0f,0
+            ),
+            Adapters.NetworkStats.TransferStats(
+                0f,0
+            )
+        )
+    )
+
+    val networkStatsState = _networkStatsState.asStateFlow()
+
+    private var _networkDownloadAndUploadList: MutableStateFlow<List<List<Float>>> = MutableStateFlow(listOf(emptyList(),emptyList()))
+    val networkDownloadAndUploadList: StateFlow<List<List<Float>>> = _networkDownloadAndUploadList.asStateFlow()
+
+    private val _diskReadAndWriteList = MutableStateFlow<List<List<Float>>>(listOf(emptyList(),emptyList()))
+    val diskReadAndWriteList = _diskReadAndWriteList.asStateFlow()
+    private val _diskStatsState = MutableStateFlow<Adapters.DiskStats>(
+        Adapters.DiskStats(
+            Adapters.DiskStats.TransferStats(
+                0f,0
+            ), Adapters.DiskStats.TransferStats(
+                0f,0
+            )
+        )
+    )
+    val diskStatsState = _diskStatsState.asStateFlow()
+
+    init {
+        val cpuCount = TaskManagerBinder.getEachCPUPercent(10).size
+        _cpuPercentState.value = List(cpuCount) { emptyList() }
+    }
 
     fun startBackgroundTask() {
+        startProcessViewBackgroundTask()
+        startResourceViewBackgroundTask()
+    }
+
+    fun startResourceViewBackgroundTask() {
+        if (supervisorJob.isCancelled) {
+            supervisorJob = SupervisorJob()
+            scope = CoroutineScope(Dispatchers.Default + supervisorJob)
+        }
+        // CPU
+        scope.launch {
+            while (true) {
+                try {
+                    val eachCPUPercent = TaskManagerBinder.getEachCPUPercent(200)
+                    val updatedCpuPercent = _cpuPercentState.value.mapIndexed { index, percentList ->
+                        val newPercent = eachCPUPercent.getOrNull(index) ?: 0f
+                        val updatedList = if (percentList.size >= 20) {
+                            percentList.drop(1)
+                        } else {
+                            percentList
+                        } + newPercent
+                        updatedList
+                    }
+                    _cpuPercentState.value = updatedCpuPercent
+                } catch (e: Exception) {
+                    Log.e("BackgroundTask", "Error updating CPU percent", e)
+                }
+                delay(1000)
+            }
+        }
+        // 内存和交换
+        scope.launch {
+            while (true) {
+                val memoryAndSwapInfo = TaskManagerBinder.getMemoryAndSwap()
+                _memoryAndSwap.value = memoryAndSwapInfo
+                val memory  = _memoryAndSwapList.value[0].toMutableStateList().apply {
+                    if(size >= 20) removeAt(0)
+                    add(memoryAndSwapInfo.memory.percent)
+                }.toList()
+                val swap = _memoryAndSwapList.value[1].toMutableStateList().apply {
+                    if(size >= 20) removeAt(0)
+                    add(memoryAndSwapInfo.swap.percent)
+                }.toList()
+                _memoryAndSwapList.value = listOf(memory,swap)
+                delay(1000)
+            }
+        }
+        // 网络
+        scope.launch {
+            while (true) {
+                val networkStats =
+                    TaskManagerBinder.getNetworkDownloadAndUpload(200)
+                _networkStatsState.value = networkStats
+                val download = networkDownloadAndUploadList.value[0].toMutableStateList().apply {
+                    if(size >= 20) removeAt(0)
+                    add(networkStats.download.speed)
+                }.toList()
+                val upload = networkDownloadAndUploadList.value[1].toMutableStateList().apply {
+                    if(size >= 20) removeAt(0)
+                    add(networkStats.upload.speed)
+                }.toList()
+                _networkDownloadAndUploadList.value = listOf(download,upload)
+            }
+        }
+        // 磁盘
+        scope.launch {
+            while(true) {
+                val diskStats = TaskManagerBinder.getDiskReadAndWrite(200)
+                _diskStatsState.value = diskStats
+                val read = _diskReadAndWriteList.value[0].toMutableStateList().apply {
+                    if(size >= 20) removeAt(0)
+                    add(_diskStatsState.value.read.speed)
+                }.toList()
+                val write =_diskReadAndWriteList.value[1].toMutableStateList().apply {
+                    if(size >= 20) removeAt(0)
+                    add(_diskStatsState.value.write.speed)
+                }.toList()
+                _diskReadAndWriteList.value = listOf(read,write)
+            }
+        }
+    }
+
+    fun startProcessViewBackgroundTask() {
         if (supervisorJob.isCancelled) {
             supervisorJob = SupervisorJob()
             scope = CoroutineScope(Dispatchers.Default + supervisorJob)
